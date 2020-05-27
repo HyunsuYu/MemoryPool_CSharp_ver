03a9d457b3a4f337c26a862c8287a43ee52185fc
HyunsuYu <63137431+HyunsuYu@users.noreply.github.com>
2020-05-27 18:00:30 +0900

    temp

---
+++ MemoryPool_CSharp/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MemoryPool_CSharp
+{
+    sealed public class MemoryPool_Custom : IDisposable
+    {
+        //  property
+
+        //  method
+        public MemoryPool_Custom()
+        {
+
+        }
+        public MemoryPool_Custom(byte size)
+        {
+
+        }
+        ~MemoryPool_Custom()
+        {
+
+        }
+        public void Dispose()
+        {
+
+        }
+
+        private void DIspose(bool bisDispose)
+        {
+            if(!bisDispose)
+            {
+                return;
+            }
+        }
+    }
+}
